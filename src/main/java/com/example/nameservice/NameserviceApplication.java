632ab5d993adfdd66d08570b41c6f0a57032a50d
Daisuke943 <oosabelidaisuke@icloud.com>
2024-03-26 10:24:22 +0900

    プロジェクト初期化

---
+++ src/main/java/com/example/nameservice/NameserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.nameservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameserviceApplication.class, args);
+	}
+
+}
