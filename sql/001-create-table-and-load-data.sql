f0a10245d4db710c38c400c331c85b8b59e2d30b
Daisuke943 <oosabelidaisuke@icloud.com>
2024-04-08 19:16:38 +0900

    dockerでMySQLを使えるように準備

---
+++ sql/001-create-table-and-load-data.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS names;
+
+CREATE TABLE names (
+ id int unsigned AUTO_INCREMENT,
+ name VARCHAR(20) NOT NULL,
+ PRIMARY KEY(id)
+);
+INSERT INTO names (name) VALUES ('name1');
+INSERT INTO names (name) VALUES ('name2');
+
+
+
